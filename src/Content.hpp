a00906407db83c1698b5aad3815c0be5ad2743a2
Spud <jeffrypig23@gmail.com>
2021-07-13 16:23:12 -0600

    Added structs to Content.hpp

--- src/Content.hpp
+++ src/Content.hpp
@@ -5,8 +5,65 @@
 #ifndef TUMBLRAPI_CONTENT_HPP
 #define TUMBLRAPI_CONTENT_HPP
 
+#include <string>
 
 class Content {
+
+public:
+
+	struct Theme {
+		// TODO Used by blog info
+	};
+
+	struct Text {
+		// TODO
+	};
+
+	struct Image {
+
+		/**
+		 * TODO Documentation
+		 */
+		std::string type;
+
+		/**
+		 * TODO Documentation
+		 */
+		std::string media_key;
+
+		/**
+		 * TODO Documentation
+		 */
+		unsigned int width;
+
+		/**
+		 * TODO Documentaiton
+		 */
+		unsigned int height;
+
+		/**
+		 * TODO Documentation
+		 */
+		std::string url;
+
+		/**
+		 * TODO Documentation
+		 */
+		bool has_original_dimensions = false;
+
+	};
+
+	struct Link {
+		// TODO
+	};
+
+	struct Audio {
+		// TODO
+	};
+
+	struct Video {
+		// TODO
+	};
 
 };
 
