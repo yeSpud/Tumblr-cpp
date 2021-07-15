3aa75656a26aae385ae3a9c0e1abc78f46a0ff2a
Spud <jeffrypig23@gmail.com>
2021-07-14 21:00:19 -0600

    added attribution cpp file

---
+++ src/attribution.cpp
@@ -0,0 +1,46 @@
+//
+// Created by Spud on 7/14/21.
+//
+
+#include <string>
+#include "attribution.hpp"
+
+struct attribution::Attribution {
+
+	/**
+	 * The type of the attribution.
+	 * Current valid values are "link", "blog", "post", or "app".
+	 */
+	attributionType type;
+
+	/**
+	 * The URL to be attributed.
+	 */
+	std::string url;
+
+	/**
+	 * A Post with at least the id field.
+	 */
+	post::Post post;
+
+	/**
+	 * A Blog with at least the uuid field.
+	 */
+	blog::Blog blog;
+
+	/**
+	 * The name of the application to be attributed.
+	 */
+	std::string app_name;
+
+	/**
+	 * Any display text that the client should use with the attribution.
+	 */
+	std::string display_text;
+
+	/**
+	 * A specific logo that the client should use with the third-party app attribution.
+	 */
+	media::MediaObject logo;
+
+};
