36c5ef2f3a5e6b4924b01c976a6cd299f366e6fc
Spud <jeffrypig23@gmail.com>
2021-07-14 21:28:01 -0600

    added link cpp file

--- include/link.hpp
+++ include/link.hpp
@@ -5,6 +5,8 @@
 #ifndef TUMBLRAPI_LINK_HPP
 #define TUMBLRAPI_LINK_HPP
 
+#include "rapidjson/document.h"
+
 class link {
 
 public:
@@ -12,44 +14,7 @@ public:
 	/**
 	 * TODO Documentation
 	 */
-	struct Link {
-
-		/**
-		 * The URL to use
-		 */
-		std::string url;
-
-		/**
-		 * The title of where the link goes.
-		 */
-		std::string title;
-
-		/**
-		 * The description of where the link goes.
-		 */
-		std::string description;
-
-		/**
-		 * The author of the link's content.
-		 */
-		std::string author;
-
-		/**
-		 * The name of the site being linked to.
-		 */
-		std::string site_name;
-
-		/**
-		 * TODO Documentation
-		 */
-		std::string display_url;
-
-		/**
-		 * An image object to use as a "poster" for the link.
-		 */
-		Image poster;
-
-	};
+	struct Link;
 
 	/**
 	 * TODO Documentation
---
+++ src/link.cpp
@@ -0,0 +1,46 @@
+//
+// Created by Spud on 7/14/21.
+//
+
+#include <string>
+#include "image.hpp"
+#include "link.hpp"
+
+struct link::Link {
+
+	/**
+	 * The URL to use
+	 */
+	std::string url;
+
+	/**
+	 * The title of where the link goes.
+	 */
+	std::string title;
+
+	/**
+	 * The description of where the link goes.
+	 */
+	std::string description;
+
+	/**
+	 * The author of the link's content.
+	 */
+	std::string author;
+
+	/**
+	 * The name of the site being linked to.
+	 */
+	std::string site_name;
+
+	/**
+	 * TODO Documentation
+	 */
+	std::string display_url;
+
+	/**
+	 * An image object to use as a "poster" for the link.
+	 */
+	image::Image poster;
+
+};
