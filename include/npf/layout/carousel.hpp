d0daf559eb25fd86f627ee30fde782f5d7c8572f
Spud <jeffrypig23@gmail.com>
2021-07-17 15:05:37 -0600

    Added fields to carousel.hpp

--- include/npf/layout/carousel.hpp
+++ include/npf/layout/carousel.hpp
@@ -5,4 +5,21 @@
 #ifndef TUMBLRAPI_CAROUSEL_HPP
 #define TUMBLRAPI_CAROUSEL_HPP
 
+#include "rows.hpp"
+
+class Carousel : public Rows {
+
+public:
+
+	Carousel()= default;;
+
+	/**
+	 * The last block to display before the Read More signifier. Required if blocks is not supplied.
+	 */
+	int truncate_after;
+
+	void populateNPF(JSON_OBJECT entry) override;
+
+};
+
 #endif //TUMBLRAPI_CAROUSEL_HPP
