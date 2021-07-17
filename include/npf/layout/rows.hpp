e1505c5e3958366938cfa5c26520bea34473cb01
Spud <jeffrypig23@gmail.com>
2021-07-17 15:00:08 -0600

    Added fields to rows.hpp

--- include/npf/layout/rows.hpp
+++ include/npf/layout/rows.hpp
@@ -5,4 +5,19 @@
 #ifndef TUMBLRAPI_ROWS_HPP
 #define TUMBLRAPI_ROWS_HPP
 
+#include "layout.hpp"
+
+class Rows : public Layout {
+
+public:
+
+	Rows(): Layout(rows) {};
+
+	// TODO
+	// auto mode;
+
+	void populateNPF(JSON_OBJECT entry) override;
+
+};
+
 #endif //TUMBLRAPI_ROWS_HPP
