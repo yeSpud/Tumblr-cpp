8c34f0b14e3b137c675dc31259aa5f053abf7e06
Spud <jeffrypig23@gmail.com>
2021-07-17 20:13:23 -0600

    added content to trail.hpp

--- include/npf/trail.hpp
+++ include/npf/trail.hpp
@@ -5,4 +5,51 @@
 #ifndef TUMBLRAPI_TRAIL_HPP
 #define TUMBLRAPI_TRAIL_HPP
 
+#include "post.hpp"
+
+
+/**
+ * TODO Documentation
+ */
+class Trail : public NPF {
+
+public:
+
+	/**
+	 * TODO Documentation
+	 */
+	Trail(): NPF(){};
+
+	/**
+	 * TODO Documentation
+	 */
+	Post post;
+
+	/**
+	 * TODO Documentation
+	 */
+	Blog blog;
+
+	/**
+	 * TODO Documentation
+	 */
+	std::vector<Content*> content;
+
+	/**
+	 * TODO Documentation
+	 */
+	std::vector<Layout*> layout;
+
+	/**
+	 * TODO Documentation
+	 */
+	std::string broken_blog_name;
+
+	/**
+	 * TODO Documentation
+	 */
+	void populateNPF(JSON_OBJECT) override;
+
+};
+
 #endif //TUMBLRAPI_TRAIL_HPP
