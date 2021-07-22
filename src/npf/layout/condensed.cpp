a85ddbd861e73a29fe5b501e25c3189699e0e698
Spud <jeffrypig23@gmail.com>
2021-07-21 18:46:58 -0600

    Added functionality to condensed

--- src/npf/layout/condensed.cpp
+++ src/npf/layout/condensed.cpp
@@ -4,8 +4,19 @@
 
 #include "npf/layout/condensed.hpp"
 
+void Condensed::populateBlocks(const JSON_ARRAY &array) { // TODO Comments
+	blocks = std::vector<int>(array.Size());
+	for (JSON_ARRAY_ENTRY &entry : array) {
+		if (entry.IsInt()) {
+			blocks.push_back(entry.GetInt());
+		}
+	}
+}
+
 void Condensed::populateNPF(JSON_OBJECT entry) { // TODO Comments
 
-	// TODO
+	POPULATE_ARRAY(entry, "blocks", populateBlocks(entry["blocks"].GetArray());)
+
+	objectHasValue(entry, "truncate_after", truncate_after);
 
 }
