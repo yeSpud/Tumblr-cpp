8f1112d1b72b06d480d617093f09281063050d50
Spud <jeffrypig23@gmail.com>
2021-07-24 20:53:23 -0600

    Added missing classes
    They still need to be implemented though oops

---
+++ tests/rowsTest.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Spud on 7/24/21.
+//
+
+#ifndef TUMBLRAPI_ROWSTEST_HPP
+#define TUMBLRAPI_ROWSTEST_HPP
+
+
+class rowsTest {
+
+};
+
+
+#endif //TUMBLRAPI_ROWSTEST_HPP
---
+++ tests/trailTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Spud on 7/24/21.
+//
+
+#include "trailTest.hpp"
