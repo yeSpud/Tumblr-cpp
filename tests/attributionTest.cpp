224073034a52c22b3f40192c24036686273771b9
Spud <jeffrypig23@gmail.com>
2021-09-16 01:14:47 -0700

    Added test for attribution

--- tests/attributionTest.cpp
+++ tests/attributionTest.cpp
@@ -4,6 +4,7 @@
 
 #include "catch2/catch.hpp"
 #include "attributionTest.hpp"
+#include "mediaTest.hpp"
 #include <iostream>
 
 void
@@ -21,20 +22,43 @@ AttributionTest::testAttribution(const Attribution &attribution, const attributi
 
 }
 
-/*
-{
-	"type": "app",
-	"url": "https://www.instagram.com/p/BVZyxTklQWX/",
-	"app_name": "Instagram",
-	"display_text": "tibbythecorgi - Very Cute", // or "Listen on Bandcamp" for audio
-	"logo": {
-		"url": "https://scontent.cdninstagram.com/path/to/logo.jpg",
-		"type": "image/jpeg",
-		"width": 64,
-		"height": 64
+TEST_CASE("Parsing Attribution Test", "[Attribution]") {
+
+	/*
+	{
+		"type": "app",
+		"url": "https://www.instagram.com/p/BVZyxTklQWX/",
+		"app_name": "Instagram",
+		"display_text": "tibbythecorgi - Very Cute",
+		"logo": {
+			"url": "https://scontent.cdninstagram.com/path/to/logo.jpg",
+			"type": "image/jpeg",
+			"width": 64,
+			"height": 64
+		}
 	}
+	 */
+
+	rapidjson::Document appAttributionDocument;
+
+	appAttributionDocument.Parse(R"({"type":"app","url":"https://www.instagram.com/p/BVZyxTklQWX/","app_name":"Instagram","display_text":"tibbythecorgi - Very Cute","logo":{"url":"https://scontent.cdninstagram.com/path/to/logo.jpg","type":"image/jpeg","width":64,"height":64}})");
+
+	REQUIRE(appAttributionDocument.IsObject());
+
+	REQUIRE(appAttributionDocument.HasMember("type"));
+	REQUIRE(appAttributionDocument.HasMember("url"));
+	REQUIRE(appAttributionDocument.HasMember("app_name"));
+	REQUIRE(appAttributionDocument.HasMember("display_text"));
+	REQUIRE(appAttributionDocument.HasMember("logo"));
+
+	Attribution appAttribution;
+	appAttribution.populateNPF(appAttributionDocument.GetObj());
+
+	AttributionTest::testAttribution(appAttribution, attribution_app, "https://www.instagram.com/p/BVZyxTklQWX/",
+									 "Instagram", "tibbythecorgi - Very Cute");
+
+	MediaTest::testMedia(appAttribution.logo, "image/jpeg", "https://scontent.cdninstagram.com/path/to/logo.jpg", 64, 64, true, false,
+	                     false);
+
+	// TODO Rest of tests (other attribution types).
 }
- */
-
-
-// TODO Rest of tests
