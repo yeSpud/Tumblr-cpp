8c302e20900b5a6aa718ac78d22f98d0fa353ab7
Spud <jeffrypig23@gmail.com>
2022-03-10 14:40:40 -0800

    Changed from http to https

--- src/TumblrAPI.cpp
+++ src/TumblrAPI.cpp
@@ -8,7 +8,7 @@ cpr::Response
 TumblrAPI::sendGetRequest(const std::string &endpoint, bool authRequired, const std::string &optionalParams) { // TODO Comments
 
 	// Format the URL to go to for retrieving data.
-	std::string url = "api.tumblr.com/v2/" + endpoint;
+	std::string url = "https://api.tumblr.com/v2/" + endpoint;
 
 	// If authorization is required add the api token.
 	if (authRequired) {
